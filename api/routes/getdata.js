6c63e658cc53f1697bec7da117b25f09681b9efa
sor973 <zerostriketeam@gmail.com>
2023-04-12 17:19:22 -0400

    getdata most recently

--- api/routes/getdata.js
+++ api/routes/getdata.js
@@ -2,19 +2,19 @@ const expressJS = require('express');
 const router = expressJS.Router();
 const Data = require('../../models/datamodel')
 
-router.get("/",async (req,res)=>{
+router.get("/", async (req, res) => {
     try {
-        const getdata = await Data.find({});
-        res.status(200).json(getdata);
-        console.log(data)
+        const latestData = await Data.find({}).sort({createdAt: -1}).limit(2);
+        res.status(200).json(latestData);
+        console.log(latestData);
     } catch (error) {
         res.status(400);
         res.json({
             error: "Bad Request Or Data not complete. ",
             message: error
         });
-        console.log(error)
+        console.log(error);
     }
-})
+});
 
 module.exports = router;
