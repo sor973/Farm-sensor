04119507e8d7de7f362506cd959e32f898e39fee
sor973 <zerostriketeam@gmail.com>
2023-04-12 18:38:39 -0400

    FInish Notify Backend

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const router = expressJS.Router();
 // API Route Import
 const recdata = require("./routes/recdata");
 const getdata = require("./routes/getdata");
+const setnotify = require("./routes/setnotify");
 
 // API Route Setting
 router.use("/recdata",recdata);
 router.use("/getdata",getdata);
+router.use("/setnotify",setnotify);
 
 // API Default Route
 router.use("/", (req,res)=>{
--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const cors = require("cors");
 const serve = require("./serve");
 const api = require("./api");
 const mongoose = require("mongoose");
+const Data = require("./models/datamodel");
+const User = require("./models/usermodel");
+const axios = require("axios");
 
-require('dotenv').config();
+require("dotenv").config();
 
+const LINE_NOTIFY_API_URL = "https://notify-api.line.me/api/notify";
+const LINE_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN;
 const DATABASE_URL = process.env.DATABASE_URL;
 const CONFIG = {
   serve: false,
@@ -22,11 +27,41 @@ function handoff() {
       res.status(403);
       res.json({ error: true, message: "This server only serve API." });
     });
+
+  const interval = setInterval(async () => {
+    try {
+      // Query the database for the desired data
+      const data = await Data.find({}).sort({ createdAt: -1 }).limit(2);
+      const status = await User.find({ userid : 1 });
+
+      if (status[0].notify == true) {
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].tempAir > status[0].tempAir || data[i].humidAir < status[0].humidAir  ) {
+            const message = `โรงเรือน ${data[i].slaveid} อุณหภูมิสูงเกิน 50 องศา!!`;
+            const headers = {
+              "Content-Type": "application/x-www-form-urlencoded",
+              Authorization: `Bearer ${LINE_ACCESS_TOKEN}`,
+            };
+            const params = new URLSearchParams();
+            params.append("message", message);
+
+            const response = await axios.post(LINE_NOTIFY_API_URL, params, {
+              headers,
+            });
+            console.log("Line notification sent:", response.data);
+          } else {
+            console.log("อุณหภูมิในโรงเรือนปกติ");
+          }
+        }
+      }
+    } catch (err) {
+      console.error("Error while checking database", err);
+    }
+  }, 5 * 60 * 1000); // 5 minutes in milliseconds
+
   mongoose.set("strictQuery", false);
   mongoose
-    .connect(
-        DATABASE_URL
-    )
+    .connect(DATABASE_URL)
     .then(() => {
       console.log("connected to MongoDB");
       app.listen(8000, () => {
