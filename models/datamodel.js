f6604b5eb9043d5b43df2e2a3843bca837752288
sor973 <zerostriketeam@gmail.com>
2023-04-13 14:27:03 -0400

    set timezone

--- models/datamodel.js
+++ models/datamodel.js
@@ -1,31 +1,34 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const dataSchema = mongoose.Schema(
-    {
-        slaveid: {
-            type: Number,
-            required: true
-        },
-        tempAir: {
-            type: Number,
-            required: true,
-            default: 0
-        },
-        humidAir: {
-            type: Number,
-            required: true,
-        },
-        humidSoil: {
-            type: Number,
-            required: true,
-        }
+  {
+    slaveid: {
+      type: Number,
+      required: true,
     },
-    {
-        timestamps: true
-    }
-)
+    tempAir: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    humidAir: {
+      type: Number,
+      required: true,
+    },
+    humidSoil: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+      timezone: "Asia/Bangkok",
+    },
+  }
+);
 
-
-const Data = mongoose.model('Data', dataSchema);
+const Data = mongoose.model("Data", dataSchema);
 
 module.exports = Data;
